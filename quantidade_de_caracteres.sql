23d631248b5ab1e268c5ed9c9d360dadff8ad3b7
Curio5813 <marcosglasner@gmail.com>
2024-06-06 13:52:23 +0100

    Updating SQL file quantidade_de_caracteres.sql

--- quantidade_de_caracteres.sql
+++ quantidade_de_caracteres.sql
@@ -3,6 +3,7 @@
 --- www.urionlinejudge.com.br
 --- Problem 2743
 
+/*
 CREATE TABLE people(
 	id INTEGER PRIMARY KEY,
 	name varchar(255)
@@ -16,7 +17,6 @@ VALUES
       (3, 'Ygor'),
       (4, 'Valentine'),
       (5, 'Jo');
-
+*/
   
-  /*  Execute this query to drop the tables */
-  -- DROP TABLE people; --
+SELECT p.name, LENGTH(p.name) AS length FROM people AS p ORDER BY length DESC;
